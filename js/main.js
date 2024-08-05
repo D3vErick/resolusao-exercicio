077fb7e1d02fcff61f63237a7dbc28efecf822a6
Erick <84731227+D3vErick@users.noreply.github.com>
2024-08-05 20:34:12 -0300

    fix: correção do exercício
    Uma letra em maiúsculo tava quebrando a parte de validação inteira…  🤡

--- js/main.js
+++ js/main.js
@@ -5,14 +5,20 @@ $(document).ready(function () {
     })
 });
 
-$('#telefone').mask('(00) 00000-0000');
+$('#telefone').mask('(00) 00000-0000', {
+    placeholder: '(11) 94002-8922'
+});
 
-$('#cpf').mask('000.000.000-00');
+$('#cpf').mask('000.000.000-00', {
+    placeholder: 'xxx-xxx-xxx-xx'
+});
 
-$('#cep').mask('00000-000');
+$('#cep').mask('00000-000', {
+    placeholder: '12345-678'
+});
 
 $('form').validate({
-    Rules: {
+    rules: {
         nome: {
             required: true
         },
@@ -33,14 +39,19 @@ $('form').validate({
             required: true
         },
     },
+    messages: {
+        name: 'Por favor, insira um nome válido.',
+        email: 'Por favor, insira um Email válido.',
+        telefone: 'Por favor, insira um número de telefone válido.'
+    },
     submitHandler: function (form) {
         alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
         form.reset();
     },
-    invalidHandler: function (evento, validator) {
-        let camposIncorretos = validador.numberOfInvalids();
+    invalidHandler: function (form, validator) {
+        let camposIncorretos = validator.numberOfInvalids();
         if(camposIncorretos){
-            alert("Por favor, preencha os campos para prosseguir com a compra!");
+            alert(`Há ${camposIncorretos} campos incorretos, corrija por favor.`);
         }
     }
 })
